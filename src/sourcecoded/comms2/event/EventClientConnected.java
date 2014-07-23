c0738956cb93a548f4a3cb65fb03363267234cf1
MSourceCoded <sourcecodedcreations@gmail.com>
2014-07-24 05:39:58 +0800

    Fixed compile error

--- src/sourcecoded/comms2/event/EventClientConnected.java
+++ src/sourcecoded/comms2/event/EventClientConnected.java
@@ -1,7 +1,5 @@
 package sourcecoded.comms2.event;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
-import sourcecoded.comms2.network.SCSide;
 import sourcecoded.comms2.network.socket.SCClient;
 import sourcecoded.events.AbstractEvent;
 
